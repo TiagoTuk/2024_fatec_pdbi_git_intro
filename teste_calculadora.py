84c8a2eab38e5ae17192491e94800853037803c4
Tiago Gomes <tiago.barros596@gmail.com>
2024-02-27 09:56:11 -0300

    teste(git-python): funcao multiplica teste

--- teste_calculadora.py
+++ teste_calculadora.py
@@ -7,5 +7,7 @@ def main():
     print (f'{a} + {b} = {soma}')
     subtracao = calculadora.subtrair(a, b)
     print (f'{a} - {b} = {subtracao}')
+    produto = calculadora.multiplicar(a, b)
+    print(f'{a} * {b} = {produto}')
 
 main()
